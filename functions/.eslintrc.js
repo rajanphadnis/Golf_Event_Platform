f290c27ec3bdfa2a397574e9e64a18dbdcee4172
rajanphadnis <rajanphadnis@users.noreply.github.com>
2021-06-19 06:20:15 +0000

    Minify source code

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,14 +1,1 @@
-module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    quotes: ["error", "double"],
-  },
-};
+module.exports={root:!0,env:{es6:!0,node:!0},extends:["eslint:recommended","google"],rules:{quotes:["error","double"]}};
