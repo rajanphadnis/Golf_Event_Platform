b40ae9f4fb7ddfca64e890dc0d3948b6c3c5eb8f
Rajan <rajansd28@gmail.com>
2021-06-21 15:16:33 -0700

    very basic firebase auth working

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+initApp = function () {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var phoneNumber = user.phoneNumber;
+            var providerData = user.providerData;
+            user.getIdToken().then(function (accessToken) {
+                document.getElementById('sign-in-status').textContent = 'Signed in';
+                document.getElementById('signInButton').textContent = 'Sign Out';
+                document.getElementById('account-details').textContent = JSON.stringify({
+                    displayName: displayName,
+                    email: email,
+                    emailVerified: emailVerified,
+                    phoneNumber: phoneNumber,
+                    photoURL: photoURL,
+                    uid: uid,
+                    accessToken: accessToken,
+                    providerData: providerData
+                }, null, '  ');
+            });
+        } else {
+            // User is signed out.
+            document.getElementById('sign-in-status').textContent = 'Signed out';
+            document.getElementById('signInButton').textContent = 'Sign In';
+            document.getElementById('account-details').textContent = 'null';
+        }
+    }, function (error) {
+        console.log(error);
+    });
+};
+window.addEventListener('load', function () {
+    initApp()
+});
