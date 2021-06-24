7ac6ecef84cbffca3231fb19dca0501651b12c25
Rajan <rajansd28@gmail.com>
2021-06-23 17:50:39 -0700

    improved auth. Added auth page rerouting

--- public/js/account.js
+++ public/js/account.js
@@ -9,27 +9,7 @@ initApp = function () {
         } else {
             signInButton.style.display = "block";
             signedInDropdown.style.display = "none";
-            // FirebaseUI config.
-            var uiConfig = {
-                signInSuccessUrl: "/",
-                signInOptions: [
-                    // Leave the lines as is for the providers you want to offer your users.
-                    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                ],
-                // tosUrl and privacyPolicyUrl accept either url string or a callback
-                // function.
-                // Terms of service url/callback.
-                tosUrl: 'https://www.google.com',
-                // Privacy policy url/callback.
-                privacyPolicyUrl: function () {
-                    window.location.assign('https://www.google.com');
-                }
-            };
-
-            // Initialize the FirebaseUI Widget using Firebase.
-            var ui = new firebaseui.auth.AuthUI(firebase.auth());
-            ui.start('#firebaseui-auth-container', uiConfig);
+            window.location = "/sign-in?l=account";
         }
     }, function (error) {
         console.log(error);
