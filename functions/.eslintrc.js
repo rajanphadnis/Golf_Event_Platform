839acb8b92086a0463e7f9c71249c294b1876301
Rajan <rajansd28@gmail.com>
2021-06-25 11:28:52 -0700

    added cloud fxn to generate blurhash on upload

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,1 +1,7 @@
-module.exports={root:!0,env:{es6:!0,node:!0},extends:["eslint:recommended","google"],rules:{quotes:["error","double"]}};
+module.exports = {
+  root: !0,
+  env: {es6: !0, node: !0},
+  extends: ["eslint:recommended", "google"],
+  rules: {quotes: ["error", "double"]},
+};
+
