0c8c3a2fbb65a3b65a1638fc7bbc47613b610e56
Rajan <rajansd28@gmail.com>
2021-07-05 11:59:46 -0700

    added basic event management and deletion

--- public/js/myEvents/charity.js
+++ public/js/myEvents/charity.js
@@ -1,1 +1,73 @@
 console.log("charity");
+document.getElementById("charityPage").style.display = "block";
+var db = firebase.firestore();
+firebase.auth().onAuthStateChanged(
+  function (user) {
+    if (user) {
+      displayCharityEvents(user.uid);
+    } else {
+      // User is signed out.
+      alert("Sorry, something went wrong. Please try again in a few moments.");
+      window.location = "/404.html";
+    }
+  },
+  function (error) {
+    console.log(error);
+  }
+);
+
+function displayCharityEvents(uid) {
+  var mainParent = document.getElementById("charityMainContent");
+  db.collection("/upcomingEvents")
+    .where("OrganizerID", "==", uid)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        var docTile = document.createElement("div");
+        var eventTitle = `<div><p>${
+          doc.data().Name
+        }</p><div>`;
+        var editbutton = `<div><a href="/my-events/edit?d=${
+          doc.id
+        }">Edit Event</a></div>`;
+        var deleteButton = `<div><a href="#" onclick="deleteEvent('${doc.id}')">Delete Event</a></div>`;
+        var longImage = `<div><img src="${
+          doc.data().ImageURL
+        }" width=150></div>`;
+
+        var tallImage = `<div><img src="${
+          doc.data().ImageURL
+        }" height=40></div>`;
+
+        docTile.className = "eventTile";
+        if (doc.data().ImageDim >= 1) {
+          docTile.innerHTML = tallImage + eventTitle + editbutton + deleteButton;
+        }
+        else {
+          docTile.innerHTML = longImage + eventTitle + editbutton + deleteButton;
+        }
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, " => ", doc.data());
+        mainParent.appendChild(docTile);
+      });
+      document.getElementById("loadingIndicator").style.display = "none";
+    })
+    .catch((error) => {
+      console.log("Error getting documents: ", error);
+    });
+}
+
+
+function deleteEvent(eventID) {
+  var canDelete = confirm("Are you sure you want to delete this event?");
+  if(canDelete) {
+    db.collection("upcomingEvents").doc(eventID).delete().then(() => {
+      console.log("Document successfully deleted!");
+      alert("Deleted Event! It may take a few minutes for the event to be fully deleted");
+      window.location = "/my-events";
+  }).catch((error) => {
+      console.error("Error removing document: ", error);
+      alert("Oh no! Something went wrong, please try again in a few minutes");
+  });
+  }
+}
--- public/js/myEvents/my-events.js
+++ public/js/myEvents/my-events.js
@@ -1,5 +1,6 @@
 var signInButton = document.getElementById("signInButton");
 var signedInDropdown = document.getElementById("signedInDropdown");
+var hasntAddedNewScript = true;
 var initApp = function () {
   var db = firebase.firestore();
   firebase.auth().onAuthStateChanged(
@@ -9,47 +10,55 @@ var initApp = function () {
         signInButton.style.display = "none";
         signedInDropdown.style.display = "flex";
         document.getElementById("accountButton").textContent = email;
-        db.collection("users")
-          .where("email", "==", user.email.toString())
-          .get()
-          .then((snap) => {
-            return snap.docs.length != 0 ? true : false;
-          })
-          .then((hasUser) => {
-            db.collection("charities")
-              .where("email", "==", user.email.toString())
-              .get()
-              .then((snap) => {
-                var hasCharity = snap.docs.length != 0 ? true : false;
-                if (hasUser) {
-                  document.getElementById("standardPage").style.display =
-                    "block";
-                  var script2 = document.createElement("script");
-                  script2.setAttribute("src", "/js/myEvents/standard.js");
-                  document.getElementById("newScript").appendChild(script2);
-                  //   var style2 = document.createElement("link");
-                  //   style2.setAttribute("rel", "stylesheet");
-                  //   style2.setAttribute("src", "/css/my-events/standard.css");
-                  //   document.getElementById("mainHead").appendChild(style2);
-                } else if (hasCharity) {
-                  document.getElementById("charityPage").style.display =
-                    "block";
-                  var script2 = document.createElement("script");
-                  script2.setAttribute("src", "/js/myEvents/charity.js");
-                  document.getElementById("newScript").appendChild(script2);
-                  //   var style2 = document.createElement("link");
-                  //   style2.setAttribute("rel", "stylesheet");
-                  //   style2.setAttribute("src", "/css/my-events/charity.css");
-                  //   document.getElementById("mainHead").appendChild(style2);
-                } else {
-                  window.location = "/onboarding?l=my-events";
-                }
-              });
-          });
+        if (hasntAddedNewScript) {
+          db.collection("users")
+            .where("email", "==", user.email.toString())
+            .get()
+            .then((snap) => {
+              return snap.docs.length != 0 ? "true" : "false";
+            })
+            .then((hasUser) => {
+              db.collection("charities")
+                .where("email", "==", user.email.toString())
+                .get()
+                .then((snap) => {
+                  var hasCharity = snap.docs.length != 0 ? true : false;
+                  if (hasUser == "true") {
+                    
+                    // document.getElementById("standardPage").style.display =
+                    //   "block";
+                    var script2 = document.createElement("script");
+                    script2.setAttribute("src", "/js/myEvents/standard.js");
+                    document.getElementById("newScript").appendChild(script2);
+                    hasAddedNewScript = false;
+                    //   var style2 = document.createElement("link");
+                    //   style2.setAttribute("rel", "stylesheet");
+                    //   style2.setAttribute("src", "/css/my-events/standard.css");
+                    //   document.getElementById("mainHead").appendChild(style2);
+                  } else if (hasCharity) {
+                    var script2 = document.createElement("script");
+                    script2.setAttribute("src", "/js/myEvents/charity.js");
+                    document.getElementById("newScript").appendChild(script2);
+                    // document.getElementById("newScript").innerHTML = '<script src="/js/myEvents/charity.js"></script>';
+                    hasAddedNewScript = false;
+                    //   var style2 = document.createElement("link");
+                    //   style2.setAttribute("rel", "stylesheet");
+                    //   style2.setAttribute("src", "/css/my-events/charity.css");
+                    //   document.getElementById("mainHead").appendChild(style2);
+                  } else {
+                    window.location = "/onboarding?l=my-events";
+                  }
+                });
+            });
+        }
+        else {
+          console.log("has already added script");
+        }
       } else {
         // User is signed out.
         signInButton.style.display = "block";
         signedInDropdown.style.display = "none";
+        window.location = "/sign-in?l=my-events";
       }
     },
     function (error) {
