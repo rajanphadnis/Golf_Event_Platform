5269bb99a702880dd0b537b6514b569216f8f002
Rajan <rajansd28@gmail.com>
2021-07-25 18:43:42 -0700

    test

--- functions/index.js
+++ functions/index.js
@@ -729,23 +729,20 @@ exports.stripePaymentDoneLetsFulfillTheOrder = functions.https.onRequest(app);
 
 app.post(
   "/webhook",
-  express.raw({ type: "application/json" }),
+  bodyParser.raw({type: 'application/json'}),
   (request, response) => {
-    const stripe = require("stripe")(
-      "sk_test_51J4urTB26mRwp60O5BbHIgEDfkczfRIK4xIrXYkwvVxTzheYbS02lEps3Y1sTlABA6q66i7WvwW3wFjeglJ7iXgq00ucGEKJPn"
-    );
-    const sig = request.headers["stripe-signature"];
-    let event;
-    const endpointSecret = "whsec_h2UmGOY7AYPHMZqcOaEbmXxNxzCBo8pC";
-    try {
-      event = stripe.webhooks.constructEvent(
-        request.rawBody,
-        sig,
-        endpointSecret
-      );
-    } catch (err) {
-      return response.status(400).send(`Webhook Error: ${err.message}`);
-    }
+    // const stripe = require("stripe")(
+    //   "sk_test_51J4urTB26mRwp60O5BbHIgEDfkczfRIK4xIrXYkwvVxTzheYbS02lEps3Y1sTlABA6q66i7WvwW3wFjeglJ7iXgq00ucGEKJPn"
+    // );
+    const sig = request.headers['stripe-signature'];
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  }
+  catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+  }
     var userDocID;
     if (event.type === "checkout.session.completed") {
       if (event.data.object.mode == "subscription") {
