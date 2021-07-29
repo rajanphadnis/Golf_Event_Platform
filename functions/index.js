b587841899eecaaa7bf37298c59fcd97ac7109ed
Rajan <rajansd28@gmail.com>
2021-07-28 19:59:11 -0700

    remove toString

--- functions/index.js
+++ functions/index.js
@@ -848,7 +848,10 @@ app3.post(
     }
     var userDocID;
     if (event.type === "customer.subscription.updated") {
-      console.log(`data: ${event.data.object.pause_collection.toString()}`);
+      console.log(`data: ${event.data.object.pause_collection}`);
+      // if (event.data.object.pause_collection != null) {
+
+      // }
       return response.status(200).send({ done: false }); 
     }
   }
