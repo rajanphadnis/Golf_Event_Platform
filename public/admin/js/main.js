ff5e81635dca25974bc3a2a56924036fa58d24cc
Rajan <rajansd28@gmail.com>
2021-08-01 21:44:46 -0700

    started to add admin console structure

--- public/admin/js/main.js
+++ public/admin/js/main.js
@@ -1,27 +1,65 @@
 initApp = function () {
-    firebase.auth().onAuthStateChanged(
-      function (user) {
-        if (user) {
-            var db = firebase.firestore();
-            db.collection("admin").doc("general").get().then((adminDoc) => {
-                if (!adminDoc.data().emails.includes(user.email)) {
-                    document.getElementById("loadingMessage").innerHTML = "not approved";
-                    // window.location = "/sign-in";
-                }
-                else {
-                    document.getElementById("loadingMessage").innerHTML = "not loading";
-                }
-            });
-        } else {
-          window.location = "/sign-in";
-        }
-      },
-      function (error) {
-        console.log(error);
+  firebase.auth().onAuthStateChanged(
+    function (user) {
+      if (user) {
+        var db = firebase.firestore();
+        db.collection("admin")
+          .doc("general")
+          .get()
+          .then((adminDoc) => {
+            if (!adminDoc.data().emails.includes(user.email)) {
+              document.getElementById("loadingMessage").innerHTML =
+                "not approved";
+              // window.location = "/sign-in";
+            } else {
+              document.getElementById("loadingMessage").innerHTML =
+                "not loading";
+            }
+          });
+      } else {
+        window.location = "/sign-in";
       }
-    );
-  };
-  window.addEventListener("load", function () {
-    initApp();
-  });
-  
+    },
+    function (error) {
+      console.log(error);
+    }
+  );
+};
+window.addEventListener("load", function () {
+  initApp();
+});
+
+function signOut() {
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      window.location = "/";
+    })
+    .catch(function (error) {
+      alert("Something happened. Try again in a few minutes");
+    });
+}
+
+function navigate(page) {
+    columnTwo = document.getElementById("columnTwo");
+    switch (page) {
+        case "users":
+            columnTwo.innerHTML = "USERS";
+            break;
+        case "events":
+            columnTwo.innerHTML = "EVENTS";
+            break;
+        case "pay":
+            columnTwo.innerHTML = "PAYMENT MANAGEMENT";
+            break;
+        case "legal":
+            columnTwo.innerHTML = "LEGAL EDITOR";
+            break;
+        case "cost":
+            columnTwo.innerHTML = "COST SUMMARY";
+            break;
+        default:
+            break;
+    }
+}
