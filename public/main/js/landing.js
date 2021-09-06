19e80dd3380987dc08b31dc0d6d0d67f11c0165d
Rajan <rajansd28@gmail.com>
2021-09-06 00:31:25 -0700

    added landgin page switching

--- public/main/js/landing.js
+++ public/main/js/landing.js
@@ -1,6 +1,64 @@
-initApp = function() {
-    
-};
-window.addEventListener("load", function() {
-    initApp();
+var hash = location.hash;
+switch (hash) {
+    case "#about":
+        document.getElementById("MainContent").innerHTML = "";
+        document.getElementById("MainContent").appendChild(document.importNode(
+            document.getElementById("about").content,
+            true
+        ));
+        break;
+    case "#pricing":
+        document.getElementById("MainContent").innerHTML = "";
+        document.getElementById("MainContent").appendChild(document.importNode(
+            document.getElementById("pricing").content,
+            true
+        ));
+        break;
+    case "#faq":
+        document.getElementById("MainContent").innerHTML = "";
+        document.getElementById("MainContent").appendChild(document.importNode(
+            document.getElementById("faq").content,
+            true
+        ));
+        break;
+    default:
+        document.getElementById("MainContent").innerHTML = "";
+        document.getElementById("MainContent").appendChild(document.importNode(
+            document.getElementById("home").content,
+            true
+        ));
+        break;
+}
+window.addEventListener('hashchange', function(e) {
+    var hash = location.hash;
+    switch (hash) {
+        case "#about":
+            document.getElementById("MainContent").innerHTML = "";
+            document.getElementById("MainContent").appendChild(document.importNode(
+                document.getElementById("about").content,
+                true
+            ));
+            break;
+        case "#pricing":
+            document.getElementById("MainContent").innerHTML = "";
+            document.getElementById("MainContent").appendChild(document.importNode(
+                document.getElementById("pricing").content,
+                true
+            ));
+            break;
+        case "#faq":
+            document.getElementById("MainContent").innerHTML = "";
+            document.getElementById("MainContent").appendChild(document.importNode(
+                document.getElementById("faq").content,
+                true
+            ));
+            break;
+        default:
+            document.getElementById("MainContent").innerHTML = "";
+            document.getElementById("MainContent").appendChild(document.importNode(
+                document.getElementById("home").content,
+                true
+            ));
+            break;
+    }
 });
